5e3e7076e725d561bf14f7908f657d3655321976
Marcel Widmer <marzelwidmer@gmail.com>
2018-05-05 17:59:56 +0200

    init commit

---
+++ src/test/kotlin/ch/keepcalm/keepcalmfood/KeepcalmFoodApplicationTests.kt
@@ -0,0 +1,16 @@
+package ch.keepcalm.keepcalmfood
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KeepcalmFoodApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
