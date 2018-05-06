89a1f8c6f248f134b30ae46e3248ccf8df11865c
Marcel Widmer <marzelwidmer@gmail.com>
2018-05-06 08:21:13 +0200

    add cvs import export sample classes

--- src/main/kotlin/ch/keepcalm/keepcalmfood/KeepcalmFoodApplication.kt
+++ src/main/kotlin/ch/keepcalm/keepcalmfood/KeepcalmFoodApplication.kt
@@ -1,11 +1,25 @@
 package ch.keepcalm.keepcalmfood
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class KeepcalmFoodApplication
+class KeepcalmFoodApplication{
+
+    @Bean
+    fun init() = CommandLineRunner {
+       print("command runner....")
+
+
+    }
+
+}
 
 fun main(args: Array<String>) {
     runApplication<KeepcalmFoodApplication>(*args)
 }
+
+
+
