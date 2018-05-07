2104f19208787a27417dbe3e3fd1327152194219
Marcel Widmer <marzelwidmer@gmail.com>
2018-05-07 20:41:11 +0200

    add db loader dev profile

--- src/main/kotlin/ch/keepcalm/keepcalmfood/KeepcalmFoodApplication.kt
+++ src/main/kotlin/ch/keepcalm/keepcalmfood/KeepcalmFoodApplication.kt
@@ -6,21 +6,33 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class KeepcalmFoodApplication(val foodService: FoodService, val foodRepository: FoodRepository) {
 
     @Bean
     fun init() = CommandLineRunner {
-       print("command runner....")
         val count = foodRepository.count()
-        foodService.loadDatabase()
         println("found $count records in database after db load")
     }
-
 }
 
 fun main(args: Array<String>) {
     runApplication<KeepcalmFoodApplication>(*args)
 }
+
+@Component
+@Profile("dev")
+class DevDatabaseLoader(val foodService: FoodService, val foodRepository: FoodRepository){
+
+      init{
+        foodService.loadDatabase()
+        val count = foodRepository.count()
+        println("found $count records in database after db load")
+      }
+
+}
+
 
